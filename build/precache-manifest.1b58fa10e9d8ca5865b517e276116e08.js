60b3f02c7fb48adf84df2f65103a8a859086b455
Sayendra Nadh Chowdary <sayendranadhchowdary@Sayendras-MacBook-Air.local>
2020-04-10 12:03:02 +0530

    new

---
+++ build/precache-manifest.1b58fa10e9d8ca5865b517e276116e08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e50cc21a0b5182b1807a95c6c3e052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8a18e3dccbb15dd9130",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "42731a1eeaf2191f9b3f",
+    "url": "/static/js/2.47463ccb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.47463ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a18e3dccbb15dd9130",
+    "url": "/static/js/main.d95376d8.chunk.js"
+  },
+  {
+    "revision": "82d564c347e8ebc08982",
+    "url": "/static/js/runtime-main.f648b2bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
